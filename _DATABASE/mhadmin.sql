e2e79dd260cf429cbef9424d019b3f3479698c45
Daan van Beusekom <daanvanbeusekom@outlook.com>
2016-12-30 17:12:51 +0100

    Update mhadmin.sql
    Add my account

--- _DATABASE/mhadmin.sql
+++ _DATABASE/mhadmin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Machine: 127.0.0.1
--- Genereertijd: 30 dec 2016 om 14:45
--- Serverversie: 5.6.11
--- PHP-versie: 5.5.3
+-- Host: 127.0.0.1
+-- Gegenereerd op: 30 dec 2016 om 17:11
+-- Serverversie: 10.1.16-MariaDB
+-- PHP-versie: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databank: `mhadmin`
+-- Database: `mhadmin`
 --
-CREATE DATABASE IF NOT EXISTS `mhadmin` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mhadmin`;
 
 -- --------------------------------------------------------
 
@@ -28,18 +26,16 @@ USE `mhadmin`;
 -- Tabelstructuur voor tabel `notifications`
 --
 
-CREATE TABLE IF NOT EXISTS `notifications` (
-  `noti_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `notifications` (
+  `noti_id` int(11) NOT NULL,
   `noti_user` int(3) NOT NULL,
   `noti_cat` int(11) NOT NULL,
   `noti_title` varchar(255) NOT NULL,
-  `noti_link` varchar(255) NOT NULL,
-  PRIMARY KEY (`noti_id`),
-  UNIQUE KEY `noti_id` (`noti_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `noti_link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden uitgevoerd voor tabel `notifications`
+-- Gegevens worden geëxporteerd voor tabel `notifications`
 --
 
 INSERT INTO `notifications` (`noti_id`, `noti_user`, `noti_cat`, `noti_title`, `noti_link`) VALUES
@@ -51,22 +47,19 @@ INSERT INTO `notifications` (`noti_id`, `noti_user`, `noti_cat`, `noti_title`, `
 -- Tabelstructuur voor tabel `regkeys`
 --
 
-CREATE TABLE IF NOT EXISTS `regkeys` (
-  `regKey_id` int(11) NOT NULL AUTO_INCREMENT,
-  `regKey_regKey` varchar(50) NOT NULL,
-  PRIMARY KEY (`regKey_id`),
-  UNIQUE KEY `regKey_regKey` (`regKey_regKey`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `regkeys` (
+  `regKey_id` int(11) NOT NULL,
+  `regKey_regKey` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden uitgevoerd voor tabel `regkeys`
+-- Gegevens worden geëxporteerd voor tabel `regkeys`
 --
 
 INSERT INTO `regkeys` (`regKey_id`, `regKey_regKey`) VALUES
 (3, 'AMMqSBznm2'),
 (5, 'f2cXZW2tJh'),
-(4, 'HkezV7ObWV'),
-(2, 'n0xrjnC6ti');
+(4, 'HkezV7ObWV');
 
 -- --------------------------------------------------------
 
@@ -74,27 +67,70 @@ INSERT INTO `regkeys` (`regKey_id`, `regKey_regKey`) VALUES
 -- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(3) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `user_id` int(3) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_pass` tinytext NOT NULL,
   `user_birthday` date NOT NULL,
   `user_description` text NOT NULL,
   `user_online` tinyint(1) NOT NULL,
-  `user_image` text NOT NULL,
-  PRIMARY KEY (`user_name`),
-  UNIQUE KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='The properties of an user' AUTO_INCREMENT=6 ;
+  `user_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='The properties of an user';
 
 --
--- Gegevens worden uitgevoerd voor tabel `users`
+-- Gegevens worden geëxporteerd voor tabel `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`, `user_birthday`, `user_description`, `user_online`, `user_image`) VALUES
 (1, 'Admin', 'admin@test.nl', 'd033e22ae348aeb5660fc2140aec35850c4da997', '1998-02-10', 'Admin', 0, 'dist/img/avatar5.png'),
+(6, 'Daan van Beusekom', 'daanvanbeusekom@outlook.com', '3304b457089faeadd1b524af859cb1f4e08e9cd1', '0000-00-00', '', 0, 'dist/img/user10.jpg'),
 (5, 'Nick Verschuur', 'n.verschuur98@gmail.com', 'ad1cffa4cc7b47f132eae4ed4ccc03213461b25b', '0000-00-00', '', 0, 'dist/img/user9.jpg');
 
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`noti_id`),
+  ADD UNIQUE KEY `noti_id` (`noti_id`);
+
+--
+-- Indexen voor tabel `regkeys`
+--
+ALTER TABLE `regkeys`
+  ADD PRIMARY KEY (`regKey_id`),
+  ADD UNIQUE KEY `regKey_regKey` (`regKey_regKey`);
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_name`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `noti_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT voor een tabel `regkeys`
+--
+ALTER TABLE `regkeys`
+  MODIFY `regKey_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
