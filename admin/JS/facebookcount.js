1cf66d452592bf8c0bd3ccf06966b2a052816edb
daanvanbeusekom <daanvanbeusekom@outlook.com>
2017-01-03 01:33:48 +0100

    Instagram fix!

--- admin/JS/facebookcount.js
+++ admin/JS/facebookcount.js
@@ -1,7 +1,7 @@
 $.ajax({
     type: "GET",
     dataType: "jsonp",
-    url: "https://graph.facebook.com/v2.8/MHEventzz/?fields=fan_count&access_token=EAAIwyuNO6SkBAItuLDaEbYEZBQOGWMt5bfSSKtTlaszcTYRNiShdGCHfGZCWnD8xl04WwphWkkFQCziW8OOVOFr8FKBSmm9SdtiyuZCYQ9ELoIZAylgtx9tS2CRvRXnt50cgxQ5sQW0aVBnREwz47lmvgH1Td8cLEOPWaiulnwZDZD",
+    url: "https://graph.facebook.com/v2.8/MHEventzz/?fields=fan_count&access_token=EAAIwyuNO6SkBAMI6jjZBUZA5oQ6kYnWUduNi6iX4r2kpuuKb5wkt1g0p3NsV4s3PPnHYlO4HZBuKRDUJfLBI7nB7ih65AMdkmaAmFtAV6gwrdiX46RwzBnGQ22YxCV7rnG5onu3qlTZCWvuC1ZA42MC8Go0vPYk0ZD",
     success: function (data) {
         var facebookfollowcount = data.fan_count;
         $(".facebookcount").html(facebookfollowcount);
--- admin/JS/instagramcount.js
+++ admin/JS/instagramcount.js
@@ -1,9 +1,8 @@
 $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    url: "https://api.instagram.com/v1/users/742662953?access_token=742662953.9931b0a.749845b82385437ba0b8ab8c171c0c55&meta=200",
-    success: function (data) {
-        var instagramfollowcount = .append(data.counts.followed_by);
-        $(".instagramcount").html(instagramfollowcount);
-    }
+  type: "GET",
+  dataType: "jsonp",
+  url: "https://api.instagram.com/v1/users/742662953?access_token=742662953.9931b0a.749845b82385437ba0b8ab8c171c0c55",
+  success: function(data) {
+    $('.instagramcount').text(data.data.counts.followed_by);
+  }
 });
